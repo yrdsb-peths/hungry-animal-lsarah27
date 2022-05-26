9bf428e1f816386f8f66584fedb87c62e0174905
lsarah27 <102365206+lsarah27@users.noreply.github.com>
2022-05-26 08:44:45 -0400

    Update Butterfly.java

--- Butterfly.java
+++ Butterfly.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Butterfly here.
+ * The Butterfly, our hero.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Lynn 
+ * @version May 2022
  */
 public class Butterfly extends Actor
 {
@@ -15,5 +15,6 @@ public class Butterfly extends Actor
     public void act() 
     {
         // Add your action code here.
+        move(1);
     }    
 }
